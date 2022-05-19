6ad2ccdaefc7cc27050a6c666a504cb89e80574a
SyauqiSuhaimi <muhammadsyauqi122@gmail.com>
2022-05-19 17:59:08 +0800

    commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import {createApp} from 'vue'
+import Footer from '../views/components/Footer.vue'
+
+const app = createApp({});
+
+app.component('Footer', Footer);
+
+app.mount('#app');
--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -2,3 +2,8 @@
 <h1>Hello </h1>
 <p>my name is</p>
 <a href="/url"> about </a>
+<div id="app">
+    <Footer/>
+</div>
+
+<script src="{{ asset('js/app.js')}}"></script>
